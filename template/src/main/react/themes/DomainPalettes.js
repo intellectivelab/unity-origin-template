bc7f97f52bcdcbb97e278d756e458b026c534b3d
Ilya Chukanov <ichukanov@intellective.com>
2020-09-18 15:13:12 +0300

    added domain theme builder to the unity cra template

---
+++ template/src/main/react/themes/DomainPalettes.js
@@ -0,0 +1,19 @@
+import {Palettes} from "@intellective/core";
+
+export default {
+	...Palettes,
+	connecticut: {
+		displayName: "Connecticut",
+		palette: {
+			type: "light",
+			primary: {
+				main: "#09538B",
+				contrastText: "#FFFFFE",
+			},
+			secondary: {
+				main: "#F6A053",
+				contrastText: '#FDFFFF'
+			},
+		}
+	},
+};
