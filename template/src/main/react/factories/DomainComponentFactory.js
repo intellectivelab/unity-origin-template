f99be0bceb99779d2eccddaf06bf7544a6fe7d70
Ilya Chukanov <ichukanov@intellective.com>
2021-01-16 19:18:55 +0300

    using @intellective/analytics components

--- template/src/main/react/factories/DomainComponentFactory.js
+++ template/src/main/react/factories/DomainComponentFactory.js
@@ -1,9 +1,16 @@
 import * as R from "ramda";
 
 import {DefaultComponentFactory} from "@intellective/core";
+import {AnalyticsComponentFactory} from "@intellective/analytics";
+
+const isChart = R.propEq('type', 'chart');
+const isIndicator = R.propEq('type', 'indicator');
 
 const DomainComponentFactory = R.cond([
 	//put your custom code here
+	[isChart, AnalyticsComponentFactory],
+	[isIndicator, AnalyticsComponentFactory],
+	[R.T, DefaultComponentFactory]
 ]);
 
-export default (props) => DomainComponentFactory(props) || DefaultComponentFactory(props);
+export default DomainComponentFactory;
