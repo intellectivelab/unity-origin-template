c76fc7a38725f313bfac7c7a7d271323969b0075
Ilya Chukanov <ichukanov@intellective.com>
2021-04-28 17:49:56 +0300

    clean up

--- template/publish-release.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Tagging docker image..."
-docker image tag unity-origin/gw-app $DOCKER_REGISTRY/unity-origin/gw-app:$VERSION
-docker image tag unity-origin/gw-app $DOCKER_REGISTRY/unity-origin/gw-app:release
-
-echo "Publishing docker image..."
-docker login -u $DOCKER_USER -p $DOCKER_PASSWORD $DOCKER_REGISTRY
-docker image push $DOCKER_REGISTRY/unity-origin/gw-app:$VERSION
-docker image push $DOCKER_REGISTRY/unity-origin/gw-app:release
